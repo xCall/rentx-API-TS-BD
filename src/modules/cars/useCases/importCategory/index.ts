6f7be309a1dc4f58e00ebeb7894fc063cb081826
xCall <carlosfaita99@gmail.com>
2022-01-04 00:01:56 -0300

    Corrigido erros

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from '../../repositories/implementatios/CategoriesRepository';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 import { ImportCategoryController } from './ImportCategoryController';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
