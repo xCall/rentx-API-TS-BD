872064f34e755f8ac67d7d76a2b5ba8e709a2af5
xCall <carlosfaita99@gmail.com>
2022-01-28 00:21:15 -0300

    Continuaçao do modulo de Create Car

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -20,7 +21,42 @@ describe('Create Car', () => {
       brand: 'Brand Car',
       category_id: 'be283007-0850-404f-b2df-e586394d040a',
     });
+  });
+  it('should not be able to create a car with exists licenses plate', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Name Car1',
+        description: 'Description Car',
+        daily_rate: 12345,
+        license_plate: 'ABC-0123',
+        fine_amount: 50,
+        brand: 'Brand Car',
+        category_id: 'be283007-0850-404f-b2df-e586394d040a',
+      });
 
-    expect(carCreated).toHaveProperty('id');
+      await createCarUseCase.execute({
+        name: 'Name Car1',
+        description: 'Description Car',
+        daily_rate: 12345,
+        license_plate: 'ABC-0123',
+        fine_amount: 50,
+        brand: 'Brand Car',
+        category_id: 'be283007-0850-404f-b2df-e586394d040a',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a car with available true by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Name Available',
+      description: 'Description Car',
+      daily_rate: 12345,
+      license_plate: 'ABC-0321',
+      fine_amount: 50,
+      brand: 'Brand Car',
+      category_id: 'be283007-0850-404f-b2df-e586394d040a',
+    });
+
+    expect(car.available).toBe(true);
   });
 });
--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import { inject, injectable } from 'tsyringe';
+// import { inject, injectable } from 'tsyringe';
 
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { AppError } from '@shared/errors/AppError';
 
 interface IRequest {
   name: string;
@@ -13,12 +15,12 @@ interface IRequest {
   category_id: string;
 }
 
-@injectable()
+// @injectable()
 class CreateCarUseCase {
   constructor(
-    @inject('CarsRepository')
+    // @inject('CarsRepository')
     private carsRepository: ICarsRepository,
-  ) { }
+  ) {}
   async execute({
     name,
     description,
@@ -27,8 +29,15 @@ class CreateCarUseCase {
     fine_amount,
     brand,
     category_id,
-  }: IRequest): Promise<void> {
-    this.carsRepository.create({
+  }: IRequest): Promise<Car> {
+    const carsAlreadyExist = await this.carsRepository.findByLicensePlate(
+      license_plate,
+    );
+
+    if (carsAlreadyExist) {
+      throw new AppError('Car already exists.');
+    }
+    const car = await this.carsRepository.create({
       name,
       description,
       daily_rate,
@@ -37,6 +46,7 @@ class CreateCarUseCase {
       brand,
       category_id,
     });
+    return car;
   }
 }
 
