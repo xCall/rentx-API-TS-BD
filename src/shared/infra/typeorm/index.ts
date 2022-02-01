f8af943d7d5ef7b0672cebc0a1c2e686441816ff
xCall <carlosfaita99@gmail.com>
2022-02-01 12:37:25 -0300

    uploade de dados complementares

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,7 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (host = 'database_ignite'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
+  console.log(defaultOptions);
   return createConnection(
     Object.assign(defaultOptions, {
       host,
