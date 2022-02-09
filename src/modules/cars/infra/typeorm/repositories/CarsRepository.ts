13b8aeead7c44a05536882ab8b4b6989c4a3c504
xCall <carlosfaita99@gmail.com>
2022-02-08 22:24:26 -0300

    Corrigido erro na query cars

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -50,13 +50,13 @@ class CarsRepository implements ICarsRepository {
       .where('available = :available', { available: true });
 
     if (brand) {
-      carsQuery.andWhere('c.brand = : brand', { brand });
+      carsQuery.andWhere('c.brand = :brand', { brand });
     }
     if (name) {
-      carsQuery.andWhere('c.name = : name', { name });
+      carsQuery.andWhere('c.name = :name', { name });
     }
     if (category_id) {
-      carsQuery.andWhere('c.category_id = : category_id', { category_id });
+      carsQuery.andWhere('c.category_id = :category_id', { category_id });
     }
     const cars = await carsQuery.getMany();
 
