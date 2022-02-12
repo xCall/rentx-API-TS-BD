39637d939bf39b53f4d37f46989e244679d4fbd6
xCall <carlosfaita99@gmail.com>
2022-02-12 14:14:44 -0300

    Correçao de segurança

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,7 +2,7 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 export default async (host = 'database_ignite'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  console.log(defaultOptions);
+  console.log('🏦 successful database connection');
   return createConnection(
     Object.assign(defaultOptions, {
       host,
