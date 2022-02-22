66266b62218fa430af6bee2e1f9727cfeca523b4
xCall <carlosfaita99@gmail.com>
2022-02-21 21:52:54 -0300

    Feito contrroler UploadCarImageController

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageController.ts
@@ -4,13 +4,25 @@ import { container } from 'tsyringe';
 
 import { UploadCarImageUseCase } from './UploadCarImageUseCase';
 
+interface IFiles {
+  filename: string;
+}
+
 class UploadCarImageController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
-    const images = request.files;
+    const images = request.files as IFiles[];
     const uploadCarImageUseCase = container.resolve(UploadCarImageUseCase);
 
-    await uploadCarImageUseCase.execute()
+    const images_name = images.map(file => file.filename);
+
+    await uploadCarImageUseCase.execute({
+      car_id: id,
+      images_name,
+    });
+
+
+    return response.status(201).send();
   }
 }
 export { UploadCarImageController };
