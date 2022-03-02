2dac395b2c1def61edd5848ea913f0f04fe4f510
xCall <carlosfaita99@gmail.com>
2022-03-02 20:55:55 -0300

    Adicionado algumas melhorias em código

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,21 @@
+import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
+
+import { CreateRentalUseCase } from './CreateRentalUseCase';
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe('Create Rental', () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it('should be able to create a new rental', () => {
+    createRentalUseCase.execute({
+      user_id: '12345',
+      car_id: '154161',
+      expected_return_date: new Date(),
+    });
+  });
+});
