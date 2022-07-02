bf49cd5bf0faf07283bc990979de23ac04e32ee4
xCall <carlosfaita99@gmail.com>
2022-07-02 15:09:56 -0300

    Adicionado migration, repositories, model e interfaces de UserTokens

---
+++ src/modules/accounts/repositories/IUsersTokenRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateUserTokenDTO } from '../dtos/ICreateUserTokenDTO';
+import { UserTokens } from '../infra/typeorm/entities/UserTokens';
+
+interface IUsersTokenRepository {
+  create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens>;
+}
+
+export { IUsersTokenRepository };
